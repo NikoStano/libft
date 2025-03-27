a7acaa5ab1ee2532ac96596f47f806b535dae92f
NikoStano <140314923+NikoStano@users.noreply.github.com>
2025-03-27 21:50:35 +0100

    update v1.1

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nistanoj <nistanoj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 23:03:41 by nistanoj          #+#    #+#             */
-/*   Updated: 2025/03/11 23:11:49 by nistanoj         ###   ########.fr       */
+/*   Updated: 2025/03/27 21:50:11 by nistanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
-	unsigned char	car;
+	unsigned char	val;
 	size_t			i;
 
 	ptr = (unsigned char *)s;
-	car = (unsigned char)c;
+	val = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
-		if (ptr[i] == car)
+		if (ptr[i] == val)
 			return (&ptr[i]);
 		i++;
 	}
